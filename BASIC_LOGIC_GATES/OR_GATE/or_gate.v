ce94a7d8a68cf393c65e9f9f6deb823320edd11f
SRI KRISHNA G <127392900+SriKrishna74@users.noreply.github.com>
2023-03-25 21:15:44 +0530

    Create or_gate.v

---
+++ BASIC_LOGIC_GATES/OR_GATE/or_gate.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: SRI KRISHNA G
+// 
+// Create Date:    15:35:39 03/25/2023 
+// Design Name: LOGICAL OR GATE
+// Module Name:    OR_gate 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module OR_gate(in1,in2,out);
+input in1,in2;
+output out;
+assign out = in1+in2;
+endmodule
