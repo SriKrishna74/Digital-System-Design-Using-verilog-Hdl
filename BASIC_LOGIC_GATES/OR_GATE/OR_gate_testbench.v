8a0a13726bbe1ac3edc0a51ebb3bbf9701f386b8
SRI KRISHNA G <127392900+SriKrishna74@users.noreply.github.com>
2023-03-26 11:49:37 +0530

    Update OR_gate_testbench.v

--- BASIC_LOGIC_GATES/OR_GATE/OR_gate_testbench.v
+++ BASIC_LOGIC_GATES/OR_GATE/OR_gate_testbench.v
@@ -1,1 +1,39 @@
+module or_tb;
 
+	// Inputs
+	reg in1;
+	reg in2;
+
+	// Outputs
+	wire out;
+
+	// Instantiate the Unit Under Test (UUT)
+	OR_gate uut (
+		.in1(in1), 
+		.in2(in2), 
+		.out(out)
+	);
+
+	initial begin
+		// Initialize Inputs
+		in1 = 0;
+		in2 = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+		in1 = 1'b0;in2 = 1'b0;
+		
+		#100;
+		in1 = 1'b0;in2 = 1'b1;
+		
+		#100;
+		in1 = 1'b1;in2 = 1'b0;
+		
+		#100;
+		in1 = 1'b1;in2 = 1'b1;
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
